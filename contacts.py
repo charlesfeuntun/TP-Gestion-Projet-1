5ef3c1ab22765a77ee103fc19166386d9d3b480b
Luc Tournie <luc.tourniem@gmail.com>
2026-01-20 14:13:52 +0100

    ajout scripts python du TP

---
+++ contacts.py
@@ -0,0 +1,20 @@
+import json
+
+def ajouter_contact(contact, fichier="contacts.json"):
+    try:
+        with open(fichier, "r") as f:
+            contacts = json.load(f)
+    except FileNotFoundError:
+        contacts = []
+    contacts.append(contact)
+    with open(fichier, "w") as f:
+        json.dump(contacts, f, indent=4)
+
+def lister_contacts(fichier="contacts.json"):
+    try:
+        with open(fichier, "r") as f:
+            contacts = json.load(f)
+        for c in contacts:
+            print(c)
+    except FileNotFoundError:
+        print("Aucun contact trouvé.")
